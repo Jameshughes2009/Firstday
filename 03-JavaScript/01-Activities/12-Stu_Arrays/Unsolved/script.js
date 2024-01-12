4a2948cfb7a7c3141b272bb2af817a68e93ccf28
James Hughes <jameshughes2009@hotmail.com>
2024-01-11 19:59:21 -0500

    more class

--- 03-JavaScript/01-Activities/12-Stu_Arrays/Unsolved/script.js
+++ 03-JavaScript/01-Activities/12-Stu_Arrays/Unsolved/script.js
@@ -1,1 +1,10 @@
 // WRITE YOUR CODE HERE
+
+var studentsArr = ["Sarah" , "Orlando" , "heather", "ismeal","hung" ]
+console.log(studentsArr.length)
+
+console.log("welcone to the class" + studentsArr[0]);
+
+studentsArr[0] = "James";
+console.log ("udated first name", studentsArr[0]);
+
