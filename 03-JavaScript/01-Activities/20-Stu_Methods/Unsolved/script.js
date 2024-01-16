63e70e5c1a990b6108c4574abc67212dd80bec62
James Hughes <jameshughes2009@hotmail.com>
2024-01-15 19:38:22 -0500

    school work

--- 03-JavaScript/01-Activities/20-Stu_Methods/Unsolved/script.js
+++ 03-JavaScript/01-Activities/20-Stu_Methods/Unsolved/script.js
@@ -3,3 +3,14 @@ var planets = ["Earth", "Saturn", "Mars", "Jupiter", "Uranus", "Venus"];
 var star = "polaris";
 
 // WRITE YOUR CODE BELOW
+// It's done when the string `"Canis Major"` is added as the first element of the `constellations` array and no elements are removed. 
+
+// * It's done when `"Venus"` is removed from the `planets` array.
+
+// * It's done when the arrays `constellations` and `planets` are joined to form a new array named `galaxy`. The arrays `constellations` and `planets` should not be altered.
+
+// * It's done when the string `"polaris"` is converted into all capital letters and the new string is stored in a variable.
+
+/**
+ * unshift/shift -> first ->
+ */
