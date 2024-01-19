366726afa32a78c2762b4ff4da35d91581ff3c0d
James Hughes <jameshughes2009@hotmail.com>
2024-01-19 12:38:53 -0500

    school review

--- 03-JavaScript/01-Activities/01-Ins_Script-ConsoleLog/script.js
+++ 03-JavaScript/01-Activities/01-Ins_Script-ConsoleLog/script.js
@@ -1,2 +1,5 @@
 // External files make your code organized and easier to maintain
 console.log("This is the log for the 🔥EXTERNAL🔥JavaScript");
+
+console.log("All console log will define on webpage")
+
