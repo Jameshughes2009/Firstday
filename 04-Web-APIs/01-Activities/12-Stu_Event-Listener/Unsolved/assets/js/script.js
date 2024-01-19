35f4e4b77c22b7f5bdb1edb4943e2d4128692153
James Hughes <jameshughes2009@hotmail.com>
2024-01-18 19:26:47 -0500

    more class

--- 04-Web-APIs/01-Activities/12-Stu_Event-Listener/Unsolved/assets/js/script.js
+++ 04-Web-APIs/01-Activities/12-Stu_Event-Listener/Unsolved/assets/js/script.js
@@ -8,5 +8,17 @@ function setCounterText() {
 }
 
 // TODO: Add event listener to increment button
+incrementEl.addEventListener("click", function(){
+  console.log("inside increment button");
+  count = count + 2;
+  console.log("new count cal", count); setCounterText();
+})
 
 // TODO: Add event listener to decrement button 
+decrementEl.addEventListener("click", function(){
+  console.log("inside decremet function");
+  //if 0, content we want greater than 0
+  if (count > 0)count = count - 1;
+  console.log("dec count value", count);
+  setCounterText();
+})
