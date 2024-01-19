6a655b798781af0e89eb368791da91e0bd11b343
James Hughes <jameshughes2009@hotmail.com>
2024-01-19 13:45:01 -0500

    more school

--- 03-JavaScript/01-Activities/03-Ins_Hello-Variable/script.js
+++ 03-JavaScript/01-Activities/03-Ins_Hello-Variable/script.js
@@ -1,9 +1,16 @@
 // Declares student variable using var keyword 
 var studentName;
 
+console.log("pre", studentName); 
+//-> declared , but not assigned,
+// -> Also not declared, not assigned = undefined
+//(string variable with no values defined)
+
 // Uses assignment operator(=) to assign a value
 var studentName = "Abdul";
 var studentAge = 32;
+
+console.log("Deez Nutes", studentName); // tonya
 
 // To re-assign a variable, use only the variable's name  
 studentName = "Tonya";
@@ -18,3 +25,15 @@ console.log("My name is ");
 
 // Logs "My name is Tonya"
 console.log("My name is " + studentName);
+console.log("My Name is", studentAge);
+
+//logs the person's age and name 
+
+console.log("deez Nuts" + studentName + "ages" +studentAge)
+
+/**
+ * // Naming tips
+ * one word= lowercasae
+ * two wordds = camel case
+ * 
+ */
