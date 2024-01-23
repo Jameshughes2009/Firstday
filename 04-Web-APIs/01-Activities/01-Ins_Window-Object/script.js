deb5818ae43a3901a149832db3272373b6c00744
James Hughes <jameshughes2009@hotmail.com>
2024-01-23 10:36:32 -0500

    more API review

--- 04-Web-APIs/01-Activities/01-Ins_Window-Object/script.js
+++ 04-Web-APIs/01-Activities/01-Ins_Window-Object/script.js
@@ -1,4 +1,5 @@
 // Logs window object using this
+// if you console.log(window) you will get the same reponse as "This"
 console.log("this: ");
 console.log(this);
 
@@ -9,3 +10,9 @@ console.log(window.document);
 // Logs body of document
 console.log("document.body: ");
 console.log( document.body);
+
+console.log("Document head")
+console.log(document.head)
+//if choosing a class use the .before the text
+console.log("doc test h1",document.querySelector("h1"));
+console.log("Doc Button",document.querySelector(".btn-test"))
--- 04-Web-APIs/01-Activities/02-Stu_Window-Object/Unsolved/script.js
+++ 04-Web-APIs/01-Activities/02-Stu_Window-Object/Unsolved/script.js
@@ -1,11 +1,12 @@
 // TODO: What will the following line of code log?
-console.log(window);
+console.log("window")
+console.log(window); // window object
 
 // TODO: What will the following line of code log?
-console.log(window.document);
+console.log(window.document); //all of the HTML file 
 
 // TODO: What will the following line of code log?
-console.log(document.documentElement);
+console.log(document.documentElement);// logs all element -> and logs the root element
 
 // TODO: What will the following line of code log?
 console.log(document.head);
