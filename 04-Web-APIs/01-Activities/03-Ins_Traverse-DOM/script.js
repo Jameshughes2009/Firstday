67b83fc4ab3a79dcb7b1ae950cb7f6d895d55fc6
James Hughes <jameshughes2009@hotmail.com>
2024-01-23 11:12:03 -0500

    lesson 3 notes

--- 04-Web-APIs/01-Activities/03-Ins_Traverse-DOM/script.js
+++ 04-Web-APIs/01-Activities/03-Ins_Traverse-DOM/script.js
@@ -3,16 +3,32 @@ console.log(document.body);
 
 console.log("Children of Document Body: ")
 console.log(document.body.children);
+// all child elements
 
 console.log("First child of body: ")
 console.log(document.body.children[0]);
-
+// just the h1 tag -> 0th index
 console.log("First child of the ul: ")
 console.log(document.body.children[1].children[0]);
+//document.body.children[1] - what in ul
+//...children[0] ->li ->#First child ul
 
 // Accessing element by id
 var firstChildUl = document.getElementById("first-child-ul");
-console.log(firstChildUl)
+console.log("before style", firstChildUl) // logs entire
+
+
 
 // Setting style of element
-firstChildUl.style.color = "green";
+setTimeout(() => {
+    firstChildUl.style.color = "green";
+    console.log("changing colors")
+}, 4000)
+// firstChildUl.style.color = "green";
+
+
+setTimeout(()=> {
+    firstChildUl.style.color = "green";
+    console.log("after style", firstChildUl);
+
+}, 8000)
