5f4e502405df318873367cce7d4d7eb5da4d0566
James Hughes <jameshughes2009@hotmail.com>
2024-03-14 10:59:45 -0400

    more school

--- 11-Express/01-Activities/02-Stu_Setup/Unsolved/server.js
+++ 11-Express/01-Activities/02-Stu_Setup/Unsolved/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const path = require('path');
 
-const app = 
+const app = express();
 const PORT = 3001;
 
 // TODO: Invoke app.use() and serve static files from the '/public' folder
+
+app.use(express.static("public"));
 
 app.get('/', (req, res) => res.send('Navigate to /send or /routes'));
 
@@ -14,6 +16,10 @@ app.get('/send', (req, res) =>
 
 // TODO: Create a route that will serve up the `public/paths.html` page
 
+app.get('/send', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/paths.html'))
+  //when getting an error message you sometime must add html to the end of the weblink
+);
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
 );
