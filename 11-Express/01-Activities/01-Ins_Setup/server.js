3e29d2d8bcaa27d902cb5548844ae61b10f8bd66
James Hughes <jameshughes2009@hotmail.com>
2024-03-14 09:42:24 -0400

    class and class content

--- 11-Express/01-Activities/01-Ins_Setup/server.js
+++ 11-Express/01-Activities/01-Ins_Setup/server.js
@@ -4,17 +4,19 @@ const express = require('express');
 // Import built-in Node.js package 'path' to resolve path of files that are located on the server
 const path = require('path');
 
-// Initialize an instance of Express.js
+// Initialize an instance of Express.js written this way bc it is an extranal package
 const app = express();
 
 // Specify on which port the Express.js server will run
 const PORT = 3001;
 
 // Static middleware pointing to the public folder
-app.use(express.static('public'));
+app.use(express.static('public'));// used to connect front and back end
 
 // Create Express.js routes for default '/', '/send' and '/routes' endpoints
+// in this context the / allow you to see the page - also will alway look for html file
 app.get('/', (req, res) => res.send('Navigate to /send or /routes'));
+//dont have to specify anything for base route bec "index.html"
 
 app.get('/send', (req, res) =>
   res.sendFile(path.join(__dirname, 'public/sendFile.html'))
--- 11-Express/README.md
+++ 11-Express/README.md
@@ -2,4 +2,4 @@
 - REST, or representational state transfer, is an architecture for creating a web service link an API
 - ![RESTful API Notes](https://github.com/Jameshughes2009/Firstday/blob/main/11-Express/images/Screenshot%202024-03-12%20162037.png)
 - Making fetch() requests will be no different than making call to a 3rd-party API. The only difference is that this api will run locally
-- 
+- When ever you make changes to a sever you will in to shut it down and restart it with control c and npm start
