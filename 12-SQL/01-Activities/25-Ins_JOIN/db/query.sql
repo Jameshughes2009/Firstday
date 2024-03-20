c7f553cf9bb2fc72bd60e8d623b9e846375786b6
James Hughes <jameshughes2009@hotmail.com>
2024-03-20 19:43:54 -0400

    class notes

--- 12-SQL/01-Activities/25-Ins_JOIN/db/query.sql
+++ 12-SQL/01-Activities/25-Ins_JOIN/db/query.sql
@@ -1,3 +1,3 @@
-SELECT *
+SELECT course_names.id AS courses_id, course_names.name AS courses_name, department.id AS dept_id, department.name AS dept_name
 FROM course_names
 JOIN department ON course_names.department = department.id;
