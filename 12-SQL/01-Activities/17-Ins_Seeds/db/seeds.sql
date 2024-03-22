8dd7cb89bbdb241b90b116f21ac05c1ef85a1c8b
James Hughes <jameshughes2009@hotmail.com>
2024-03-22 10:40:03 -0400

    work for the morning

--- 12-SQL/01-Activities/17-Ins_Seeds/db/seeds.sql
+++ 12-SQL/01-Activities/17-Ins_Seeds/db/seeds.sql
@@ -4,7 +4,7 @@
 --        (003, "Calculus I", "Limits, Differentiation, Derivatives", true),
 --        (004, "Euclidean Geometry", "Intuitively Appealing Axioms Galore", false);
        
--- WIth AUTO_INCREMENT we can remove "id" and all realted numbers
+-- WIth AUTO_INCREMENT we can remove "id" and all realted numbers -- see above example
 INSERT INTO courses (course_title, course_description, active)
 VALUES ("Algebra I", "Linear equations, inequalities, functions, and graphs", true),
        ("Pre-Calculus", "Polynomials, Complex Numbers, Vectors", true),
