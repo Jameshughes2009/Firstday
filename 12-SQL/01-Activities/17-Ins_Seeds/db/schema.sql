24d75638121c4f6d12a27b0e75a04d2748a06550
James Hughes <jameshughes2009@hotmail.com>
2024-03-22 10:04:35 -0400

    more class review

--- 12-SQL/01-Activities/17-Ins_Seeds/db/schema.sql
+++ 12-SQL/01-Activities/17-Ins_Seeds/db/schema.sql
@@ -4,7 +4,8 @@ CREATE DATABASE course_db2;
 USE course_db2;
 
 CREATE TABLE courses (
-  id INT NOT NULL,
+  -- id INT NOT NULL, -- Primary key in needed why trying to auto increment
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   course_title VARCHAR(30) NOT NULL,
   course_description TEXT NOT NULL,
   active BOOLEAN NOT NULL,
