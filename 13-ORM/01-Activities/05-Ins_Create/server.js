cd6f74ddb4f088d905cedfc1ac4461e3c9e04bcf
James Hughes <jameshughes2009@hotmail.com>
2024-03-31 10:15:16 -0400

    more notes

--- 13-ORM/01-Activities/05-Ins_Create/server.js
+++ 13-ORM/01-Activities/05-Ins_Create/server.js
@@ -8,9 +8,10 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(routes);
+app.use(routes); // used to push info in routes folder
 
 // Force false so data doesn't get dropped on every sync
+// - use force: true to drop data everytime
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
