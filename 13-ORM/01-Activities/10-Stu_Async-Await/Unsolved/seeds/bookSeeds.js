9b52756feb201b5fe5aa7c47f5d33d61338fa7b6
James Hughes <jameshughes2009@hotmail.com>
2024-04-02 08:58:32 -0400

    more testing

--- 13-ORM/01-Activities/10-Stu_Async-Await/Unsolved/seeds/bookSeeds.js
+++ 13-ORM/01-Activities/10-Stu_Async-Await/Unsolved/seeds/bookSeeds.js
@@ -6,22 +6,31 @@ const Library = require('../models/Library');
 const bookSeedData = require('./bookSeedData.json');
 const librarySeedData = require('./librarySeedData.json');
 
-// TODO Use async / await to Refactor the seedDatabase function below
-const seedDatabase = () => {
-  return sequelize.sync({ 
-    force: true 
-  })
-  .then(() => {
-    Book.bulkCreate(bookSeedData)
-    .then(() => {
-      Library.bulkCreate(librarySeedData)
-      .then(() => {
-        console.log('All Seeds Planted'); 
-      });
-    });
-  });
+// TODO Use async / await to Refactor the seedDatabase function below 
+// const seedDatabase = () => {
+//   return sequelize.sync({ 
+//     force: true 
+//   })
+//   .then(() => {
+//     Book.bulkCreate(bookSeedData)
+//     .then(() => {
+//       Library.bulkCreate(librarySeedData)
+//       .then(() => {
+//         console.log('All Seeds Planted'); 
+//       });
+//     });
+//   });
 
-  process.exit(0);
-};
+//   process.exit(0);
+// };
+// - ADD the "async" keyword to the function "seedDatabase" to make Asynchronous
+const seedDatabase = async () => {
+  // Add the `await` keyword infront of the expressions inside the `async` function.
+  await sequelize.sync({ force: true});
+
+  //Once JavaScript recogonizes the `await` keyword it waits for the promise to be fufilled before moving on.
+  await Book.bulkCreaye(bookSeedData);
+
+}
 
 seedDatabase();
