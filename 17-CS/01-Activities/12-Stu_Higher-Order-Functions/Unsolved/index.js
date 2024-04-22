c34e641030c151b0b286f4714a511894a1232690
James Hughes <jameshughes2009@hotmail.com>
2024-04-22 19:01:02 -0400

    12 stu notes

--- 17-CS/01-Activities/12-Stu_Higher-Order-Functions/Unsolved/index.js
+++ 17-CS/01-Activities/12-Stu_Higher-Order-Functions/Unsolved/index.js
@@ -3,12 +3,15 @@ const grades = [56, 78, 99, 85];
 // TODO: Complete the 'findAverage' function below.
 function findAverage(accumulator, currentValue, index, array) {
     if (index === array.length -1){
+        // dividing by how many elements are in the give arrey = 4
         return (accumulator + currentValue) / array.length;
     }
     return accumulator + currentValue;
+    //add the current value to our total 
 }
 
 // TODO: Pass the 'findAverage' into the reduce method.
 let gradeAverage = grades.reduce(findAverage);
 
+//see results with a consle log
 console.log(gradeAverage);
