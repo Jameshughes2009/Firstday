e21b11fb6e583678204a7362ee1f160c040afc46
James Hughes <jameshughes2009@hotmail.com>
2024-04-22 19:15:12 -0400

    working with clousures 13

--- 17-CS/01-Activities/12-Stu_Higher-Order-Functions/Unsolved/index.js
+++ 17-CS/01-Activities/12-Stu_Higher-Order-Functions/Unsolved/index.js
@@ -3,7 +3,7 @@ const grades = [56, 78, 99, 85];
 // TODO: Complete the 'findAverage' function below.
 function findAverage(accumulator, currentValue, index, array) {
     if (index === array.length -1){
-        // dividing by how many elements are in the give arrey = 4
+        // dividing by how many elements are in the give arrey = 4 the number will increase with addded values
         return (accumulator + currentValue) / array.length;
     }
     return accumulator + currentValue;
--- 17-CS/01-Activities/13-Ins_Closures/index.js
+++ 17-CS/01-Activities/13-Ins_Closures/index.js
@@ -2,8 +2,8 @@
 function bankAccount() {
   // Create two variables inside of the outer function.
   // We will be accessing the two variables inside of our inner function.
-  const checking = 400;
-  const savings = 1000;
+  let checking = 400; // need to start using let instend of const for variables
+  let savings = 1000;
 
   // Return a newly created inner function.
   return {
@@ -14,6 +14,9 @@ function bankAccount() {
         `You have $${checking} in your checking account and $${savings} in your savings account`
       );
     },
+    addToChecking: function(amount) {
+      checking += amount
+    }
   };
 }
 
@@ -21,11 +24,17 @@ function bankAccount() {
 const myBank = bankAccount();
 
 // With our newly created variable call the `displayFunds` method attached to it.
-myBank.displayFunds();
+
+myBank.displayFunds(); // order 1
+
+myBank.addToChecking(765); // 2
 
 // Check the console and expand the given object -> displayFunds -> Scope and then you should be able to visually see your closures.
-console.dir(myBank);
+myBank.displayFunds(); //3
+
+console.dir(myBank); //4
+// myBank.displayFunds
 
 // By console logging the outer function's variable we can see that the variables are not able to be accessed.
-console.log(checking);
-console.log(savings);
+// console.log(checking);
+// console.log(savings);
