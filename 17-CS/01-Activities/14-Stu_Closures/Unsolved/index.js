bed36e30e5ffdd7156077d63a6176250fc7afc59
James Hughes <jameshughes2009@hotmail.com>
2024-04-22 20:24:58 -0400

    all gode:

--- 17-CS/01-Activities/14-Stu_Closures/Unsolved/index.js
+++ 17-CS/01-Activities/14-Stu_Closures/Unsolved/index.js
@@ -1,4 +1,29 @@
 // TODO: Complete the 'counter' function below.
-function counter() {}
+function counter() {
 
+    // let money = 9000;
+    // let gold = 100000;
+
+    // return{
+    //     displayMoney: function() {
+    //         console.log(`you have this much $${money} and this much $${gold}`);
+    //     },
+    //     addToMoney: function(amount) {
+    //         money += amount
+    //     }
+    // }
+    let count = 0;
+
+    return {
+        increment: function() {
+            return ++count;
+        }
+    }
+}
+
+// const myBank = counter();
+
+// myBank.displayMoney();
+console.log(counter)
 module.exports = counter;
+
