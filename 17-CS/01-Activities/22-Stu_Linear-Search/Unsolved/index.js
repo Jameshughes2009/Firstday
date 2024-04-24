879af695f00b5610927fe045cf16c6979c80afd3
James Hughes <jameshughes2009@hotmail.com>
2024-04-24 19:05:15 -0400

    class notes

--- 17-CS/01-Activities/22-Stu_Linear-Search/Unsolved/index.js
+++ 17-CS/01-Activities/22-Stu_Linear-Search/Unsolved/index.js
@@ -1,4 +1,16 @@
 const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 7];
 
 // TODO: Complete the linearSearch function below.
-const linearSearch = (array, element) => {};
+const linearSearch = (array, element) => {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === element) {
+            results.push(i);
+        }
+    } if (results.length === 0) {
+        results.push(-1);
+    }
+
+    return results;
+};
+
+console.log(linearSearch(arr,7));
