7c2dd8237147c16340104f3f6d09651070bf84b6
James Hughes <jameshughes2009@hotmail.com>
2024-04-24 19:30:13 -0400

    more class

--- 17-CS/01-Activities/21-Ins_Linear-Search/index.js
+++ 17-CS/01-Activities/21-Ins_Linear-Search/index.js
@@ -1,3 +1,19 @@
 // const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 // const linearSearch = (array, element) {}
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 7];
+
+// TODO: Complete the linearSearch function below.
+const linearSearch = (array, element) => {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === element) {
+            results.push(i);
+        }
+    } if (results.length === 0) {
+        results.push(-1);
+    }
+
+    return results;
+};
+
+console.log(linearSearch(arr,7));
--- 17-CS/01-Activities/24-Stu_Recursion/Unsolved/index.js
+++ 17-CS/01-Activities/24-Stu_Recursion/Unsolved/index.js
@@ -4,10 +4,20 @@ const fibonacci = (position) => {
   if (position < 2) {
     return position;
   }
+  //This conditional statement checks if the position provided is less than 2. If it is, the function returns the position itself. This likely serves as the base case for the Fibonacci sequence, where the first two numbers are typically 0 and 1.
 
   // TODO: Add a comment describing the purpose of this return statement.
   return fibonacci(position - 1) + fibonacci(position - 2);
-};
+}; // position is the variable in the line as it occurs 
 
 // TODO: What will this return?
-console.log(fibonacci(9));
+console.log(fibonacci(11));
+// 34
+//The return statement within the fibonacci function calculates the Fibonacci number for a given position by recursively summing the values of the Fibonacci numbers at positions position - 1 and position - 2.
+
+//For example, fibonacci(9) will recursively compute the Fibonacci numbers until it reaches the base case (position < 2), and then it will start summing up the values for positions 8 and 7, then 7 and 6, and so on until it reaches the base case for each recursive call. Finally, it will return the sum of Fibonacci numbers at positions 8 and 7.
+
+//So, fibonacci(9) will return 34.
+// alwast add the last values together
+
+// 0, 1, 2, 3, 5, 8, 13, 21, 34, 55
