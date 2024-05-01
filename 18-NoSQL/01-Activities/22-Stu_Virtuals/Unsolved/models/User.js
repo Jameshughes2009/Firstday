784f147b395df33b8e621a54fe903d0848ecc66f
James Hughes <jameshughes2009@hotmail.com>
2024-05-01 19:46:32 -0400

    school

--- 18-NoSQL/01-Activities/22-Stu_Virtuals/Unsolved/models/User.js
+++ 18-NoSQL/01-Activities/22-Stu_Virtuals/Unsolved/models/User.js
@@ -22,8 +22,18 @@ userSchema.virtual('fullName').get(function () {
 // TODO: Create a getter for the virtual that returns the full name of the user (first + last)
 router.route('/:userId').get();
 // TODO: Create a setter for the virtual that sets the value of the first and last name, given just the `fullName`
-
+userSchema
+  .virtual('fullName')
+// getter
+  .get(function() {
+    return`${this.first} ${this.last}`
+  })
+  .set(function (nameValue) {
+    //eg: nameValue = "abc xyz"
+    const fName = nameValue.split('')
+  })
 // Initialize our User model
 const User = model('user', userSchema);
 
 module.exports = User;
+//https://mongoosejs.com/docs/tutorials/virtuals.html
