b8d29d654b292cd0eafb2cafc8ad3d56f00c8c75
James Hughes <jameshughes2009@hotmail.com>
2024-05-08 21:10:03 -0400

    mini project

--- 20-React/01-Activities/28-Stu_Mini-Project/Develop/src/components/Bucket.jsx
+++ 20-React/01-Activities/28-Stu_Mini-Project/Develop/src/components/Bucket.jsx
@@ -11,6 +11,8 @@ function Bucket(props) {
   console.log(props.bucket);
 
   const submitUpdate = (value) => {
+    props.editBucketItem(edit.id, value);
+    setEdit({ id: null, value: '', eagerness: ''})
 
     // TODO: Write logic to call the editBucketItem prop with the supplied values
 
@@ -23,24 +25,39 @@ function Bucket(props) {
     return <BucketForm edit={edit} onSubmit={submitUpdate} />;
   }
 
-  return props.bucket.map((item, index) => (
-    // TODO: Add a className of `bucket-row complete ${item.eagerness}` for completed items, and `bucket-row ${item.eagerness}` for non-completed items
-    // TODO: Add a key attribute set to the value of the index position
-    // Hint: use a ternary operator
-    <div className={} key={}>
+//   return props.bucket.map((item, index) => (
+//     // TODO: Add a className of `bucket-row complete ${item.eagerness}` for completed items, and `bucket-row ${item.eagerness}` for non-completed items
+//     // TODO: Add a key attribute set to the value of the index position
+//     // Hint: use a ternary operator
+//     <div className={} key={}>
 
-      {/* TODO: Add an onClick event that invokes the `completeBucketItem` method passing the item id as a argument */}
-      <div key={} onClick={}>
-          {/* TODO: Add the item text here */}
-      </div>
-      <div className="icons">
-        {/* TODO: Add an onClick event update the `edit` object with the `id`, `value`, and `eagerness` properties */}
-        <p onClick={}> ✏️</p>
-        {/* TODO: Add an onClick event that will invoke the removeBucketItem method passing in the `item.id` */}
-        <p onClick={}> 🗑️</p>
-      </div>
+//       {/* TODO: Add an onClick event that invokes the `completeBucketItem` method passing the item id as a argument */}
+//       <div key={} onClick={}>
+//           {/* TODO: Add the item text here */}
+//       </div>
+//       <div className="icons">
+//         {/* TODO: Add an onClick event update the `edit` object with the `id`, `value`, and `eagerness` properties */}
+//         <p onClick={}> ✏️</p>
+//         {/* TODO: Add an onClick event that will invoke the removeBucketItem method passing in the `item.id` */}
+//         <p onClick={}> 🗑️</p>
+//       </div>
+//     </div>
+//   ));
+// }
+return props.bucket.map((item, index) => (
+  <div className={`bucket-row ${item.completed ? 'complete' : ''} ${item.eagerness}`} key={index}>
+    {/* Adding an onClick event that invokes the completeBucketItem method passing the item id as an argument */}
+    <div key={item.id} onClick={() => props.completeBucketItem(item.id)}>
+      {/* Adding the item text here */}
+      {item.value}
     </div>
-  ));
+    <div className="icons">
+      {/* Adding an onClick event to update the edit object with the id, value, and eagerness properties */}
+      <p onClick={() => setEdit({ id: item.id, value: item.value, eagerness: item.eagerness })}> ✏️</p>
+      {/* Adding an onClick event that will invoke the removeBucketItem method passing in the item.id */}
+      <p onClick={() => props.removeBucketItem(item.id)}> 🗑️</p>
+    </div>
+  </div>
+));
 }
-
 export default Bucket;
--- 20-React/01-Activities/28-Stu_Mini-Project/Develop/src/components/BucketForm.jsx
+++ 20-React/01-Activities/28-Stu_Mini-Project/Develop/src/components/BucketForm.jsx
@@ -1,22 +1,109 @@
+// import { useState } from 'react';
+
+// function BucketForm(props) {
+//   const [input, setInput] = useState('');
+//   let [eagerness, setEagerness] = useState('');
+
+//   // TODO: Use this array in the return statement below
+//   const eagernessLevel = ['high', 'medium', 'low']
+
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+
+//     if (!eagerness) {
+//       eagerness = 'low';
+//     }
+
+//     props.onSubmit({
+//       id: Math.random(Math.floor() * 1000),
+//       text: input,
+//       eagerness: eagerness,
+//     });
+
+//     setInput('');
+//     setEagerness('');
+//   };
+
+//   const handleChange = (e) => {
+//     setInput(e.target.value);
+//   };
+
+//   // First we check to see if "edit" prop exists. If not, we render the normal form
+//   // If the prop "edit" exists, we know to render the update form instead
+//   return !props.edit ? (
+//     <div>
+//       <form className="bucket-form" onSubmit={handleSubmit}>
+//         <input
+//           type="text"
+//           placeholder="Add to your bucket list"
+//           value={input}
+//           name="text"
+//           className="bucket-input"
+//           onChange={handleChange}
+//         ></input>
+//         <div className="dropdown">
+//           <button className={`dropbtn ${eagerness}`}>
+//             {eagerness || 'Priority'}
+//           </button>
+//           <div className="dropdown-content">
+//             {/* TODO: Add an onClick event that will set the corresponding eagerness level from the `eagernessLevel` array */}
+//             <p onClick={}>Must do</p>
+//             <p onClick={}>Want to do</p>
+//             <p onClick={}>Take it or leave it</p>
+//           </div>
+//         </div>
+//         <button className="bucket-button">Add bucket list item</button>
+//       </form>
+//     </div>
+//   ) : (
+//     <div>
+//       <h3>Update entry: {props.edit.value}</h3>
+//       <form className="bucket-form" onSubmit={handleSubmit}>
+//         <input
+//           type="text"
+//           placeholder={props.edit.value}
+//           value={input}
+//           name="text"
+//           className="bucket-input"
+//           onChange={handleChange}
+//         ></input>
+//         <div className="dropdown">
+//           <button className={`dropbtn ${eagerness}`}>
+//             {eagerness || 'Priority'}
+//           </button>
+//           <div className="dropdown-content">
+//             {/* TODO: Add an onClick event that will set the corresponding eagerness level from the `eagernessLevel` array */}
+//             <p onClick={}>Must do</p>
+//             <p onClick={}>Want to do</p>
+//             <p onClick={}>Take it or leave it</p>
+//           </div>
+//         </div>
+//         <button className="bucket-button">Update</button>
+//       </form>
+//     </div>
+//   );
+// }
+
+// export default BucketForm;
+
 import { useState } from 'react';
 
 function BucketForm(props) {
   const [input, setInput] = useState('');
-  let [eagerness, setEagerness] = useState('');
+  const [eagerness, setEagerness] = useState('');
 
-  // TODO: Use this array in the return statement below
-  const eagernessLevel = ['high', 'medium', 'low']
+  const eagernessLevel = ['high', 'medium', 'low'];
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
     if (!eagerness) {
-      eagerness = 'low';
+      setEagerness('low');
     }
 
     props.onSubmit({
-      id: Math.random(Math.floor() * 1000),
-      text: input,
+      id: Math.random().toString(36).substr(2, 9),
+      value: input,
       eagerness: eagerness,
     });
 
@@ -28,8 +115,10 @@ function BucketForm(props) {
     setInput(e.target.value);
   };
 
-  // First we check to see if "edit" prop exists. If not, we render the normal form
-  // If the prop "edit" exists, we know to render the update form instead
+  const handleEagernessChange = (level) => {
+    setEagerness(level);
+  };
+
   return !props.edit ? (
     <div>
       <form className="bucket-form" onSubmit={handleSubmit}>
@@ -40,16 +129,17 @@ function BucketForm(props) {
           name="text"
           className="bucket-input"
           onChange={handleChange}
-        ></input>
+        />
         <div className="dropdown">
           <button className={`dropbtn ${eagerness}`}>
             {eagerness || 'Priority'}
           </button>
           <div className="dropdown-content">
-            {/* TODO: Add an onClick event that will set the corresponding eagerness level from the `eagernessLevel` array */}
-            <p onClick={}>Must do</p>
-            <p onClick={}>Want to do</p>
-            <p onClick={}>Take it or leave it</p>
+            {eagernessLevel.map((level, index) => (
+              <p key={index} onClick={() => handleEagernessChange(level)}>
+                {level}
+              </p>
+            ))}
           </div>
         </div>
         <button className="bucket-button">Add bucket list item</button>
@@ -66,16 +156,17 @@ function BucketForm(props) {
           name="text"
           className="bucket-input"
           onChange={handleChange}
-        ></input>
+        />
         <div className="dropdown">
           <button className={`dropbtn ${eagerness}`}>
             {eagerness || 'Priority'}
           </button>
           <div className="dropdown-content">
-            {/* TODO: Add an onClick event that will set the corresponding eagerness level from the `eagernessLevel` array */}
-            <p onClick={}>Must do</p>
-            <p onClick={}>Want to do</p>
-            <p onClick={}>Take it or leave it</p>
+            {eagernessLevel.map((level, index) => (
+              <p key={index} onClick={() => handleEagernessChange(level)}>
+                {level}
+              </p>
+            ))}
           </div>
         </div>
         <button className="bucket-button">Update</button>
